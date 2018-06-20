1d0496f7c3c7655a7d7093a814182fb390d1508b
pablopawpaw <lpault29@gmail.com>
2018-06-20 10:15:14 -0400

    sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :destroy, :update]
-before_action :require_login, only: [:edit, :update, :destroy]
+  before_action :require_login, only: [:edit, :update, :destroy]
 
   def new
     @user = User.new
