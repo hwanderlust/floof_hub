583719281506100c3dccad842eb72209c6f7b645
pablopawpaw <lpault29@gmail.com>
2018-06-20 21:19:49 -0400

    city state gem && updated user form with collection select for country state

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.3.3'
 gem 'carmen', '~> 1.0', '>= 1.0.2'
 gem "paperclip", "~> 6.0.0"
+gem 'city-state'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,21 +2,32 @@ class Location < ApplicationRecord
   has_many :users
   has_many :shelters
   has_many :pets, through: :shelters
-  def states
-    us = Country.named("United States")
-    states = us.subregions.typed("state").map{|state|state.code}.count
-    states << "DC"
+
+  State = Struct.new(:name, :code)
+  def self.states
+    states_array = Array.new
+    state_names = Country.named("United States").subregions.typed("state").map{|s|s.name}
+    state_names << "Washington DC"
+    state_codes = Country.named("United States").subregions.typed("state").map{|s|s.code}
+    state_codes << "DC"
+    states_array = state_codes.zip(state_names)
+    params = []
+    states_array.each do |s|
+      params << State.new(s[1], s[0])
+    end
+    params
   end
-  def countries
+
+  def self.countries
     Country.all.map{|c|c.name}
   end
-  #
-  # def self.make(params_hash)
-  #   location = Location.new(params_hash)
-  #   if location.valid?
-  #     location.save
-  #   end
-  #   location
-  # end
+
+  def self.cities(state, country_name)
+    country = CS.countries.select do |k,v|
+      v == country_name
+    end
+    country = country.keys.first.to_s
+    CS.cities(:"#{state}", :"#{country}")
+  end
 
 end
