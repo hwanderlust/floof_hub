afadba1e7c03c51aec4ca46aead3622b1837bf47
pablopawpaw <lpault29@gmail.com>
2018-06-21 15:03:44 -0400

    finalized user pf show view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_21_133832) do
+ActiveRecord::Schema.define(version: 2018_06_20_224853) do
 
   create_table "locations", force: :cascade do |t|
     t.string "country"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2018_06_21_133832) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "gender"
-    t.string "avatars"
     t.string "avatar_file_name"
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
