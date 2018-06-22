7eb246373b7dc377c6e223fb1ef53f5c736d377b
pablopawpaw <lpault29@gmail.com>
2018-06-21 20:50:27 -0400

    delete pet pic feature

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,9 +1,11 @@
 class Pet < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :shelter
-  
+
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  before_validation { avatar.clear if @delete_avatar }
 
   validates  :name, {presence: true, length: {minimum: 2, maximum: 70}}
   validates  :age, {presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 122 }}
@@ -12,4 +14,11 @@ class Pet < ApplicationRecord
   validates :breed, {presence: true, length: {minimum: 3, maximum: 64}}
   validates :temperament, {presence: true, length: {minimum: 3, maximum: 64}}
 
+  def delete_avatar
+    @delete_avatar ||= false
+  end
+
+  def delete_avatar=(value)
+    @delete_avatar  = !value.to_i.zero?
+  end
 end
